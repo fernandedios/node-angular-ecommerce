c927f1b2dfbff7a7da73ce1d7cf08ce62b23b600
Fernan de Dios <info@mediagramstudios.com>
2018-03-14 11:02:47 +0800

    added rest api service

--- client/EcomAngular/src/app/app.module.ts
+++ client/EcomAngular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +22,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [RestApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
