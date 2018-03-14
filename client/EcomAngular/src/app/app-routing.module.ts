ca024eb453ea4d62d6e232feb384237f289e62d4
Fernan de Dios <info@mediagramstudios.com>
2018-03-14 10:34:00 +0800

    added home component and route

--- client/EcomAngular/src/app/app-routing.module.ts
+++ client/EcomAngular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '', // home path
+    component: HomeComponent
+  },
+  {
+    // if no match, redirect to home ''
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
