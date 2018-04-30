4ca9cfe2c4dde3bd06abb0df0408805300589133
Fernan de Dios <info@mediagramstudios.com>
2018-04-30 12:07:56 +0800

    address component configuration

--- client/EcomAngular/src/app/address/address.component.ts
+++ client/EcomAngular/src/app/address/address.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../data.service';
+import { RestApiService } from '../rest-api.service';
+
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.scss']
 })
 export class AddressComponent implements OnInit {
+  btnDisabled = false;
+  currentAddress: any;
 
-  constructor() { }
+  constructor(private data: DataService, private rest: RestApiService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      const data = await this.rest.get(
+        'http://localhost:3030/api/accounts/address'
+      );
+
+      if (JSON.stringify(data[address]) === '{}' && this.data.message === '') {
+        this.data.warning('You have not entered your shipping address. Please enter your shipping address')
+      }
+
+      this.currentAddress = data['address'];
+    }
+    catch (error) {
+      this.data.error(error['message']);
+    }
   }
-
 }
